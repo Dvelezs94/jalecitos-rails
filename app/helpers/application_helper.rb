72f7c1ffd0cdd0739ffb6bb77894f6d6de7ae29f
mauriciovs <mauriciovs@hotmail.com>
2018-10-04 11:23:22 -0500

    added navigation links for users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def nav_links_helper
+    if logged_in?(:user)
+      render 'shared/nav_links_user'
+    else
+      render 'shared/nav_links_guest'
+    end
+  end
+
 end
