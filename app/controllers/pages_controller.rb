9a1c4392b2615b53807f21be2cf5a442a69c7784
Diego Velez <diegovelzs94@gmail.com>
2018-10-09 13:39:59 -0500

    added users to admin panel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
     if current_user && current_user.has_role?(:admin)
       @gigs = Gig.order(status: :desc).page(params[:gig_page]).per(25)
       @categories =  Category.order(:name).page(params[:category_page]).per(10)
+      @users =  User.order(:name).page(params[:user_page]).per(25)
     elsif current_user && current_user.has_role?(:user)
       @gigs = Gig.where.not(user_id: current_user.id, status: "published")
     end
