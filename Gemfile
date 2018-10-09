e5cfd71a716f66079723ba53d963c0ddee27a360
Diego Velez <diegovelzs94@gmail.com>
2018-10-09 12:33:14 -0500

    added pagination and fixed stuff

--- Gemfile
+++ Gemfile
@@ -81,3 +81,5 @@ gem 'mini_magick', '~> 4.9', '>= 4.9.2'
 gem 'aws-sdk', '~> 3.0', '>= 3.0.1'
 # FeatherIcons
 gem 'feathericon-rails', '~> 1.0'
+# Pagination
+gem 'kaminari', '~> 1.1', '>= 1.1.1'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   def home
     # Info for admin Dashboard
     if current_user && current_user.has_role?(:admin)
-      @gigs = Gig.all
-      @categories = Category.all
+      @gigs = Gig.order(status: :desc).page(params[:gig_page]).per(25)
+      @categories =  Category.order(:name).page(params[:category_page]).per(10)
     elsif current_user && current_user.has_role?(:user)
       @gigs = Gig.where.not(user_id: current_user.id, status: "published")
     end
