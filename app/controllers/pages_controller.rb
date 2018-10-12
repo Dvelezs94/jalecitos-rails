3f168b86bddceb178da32f256c63b9153b008417
Diego Velez <diegovelzs94@gmail.com>
2018-10-12 01:17:06 -0500

    fixed gigs shown

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,11 @@ class PagesController < ApplicationController
       @categories =  Category.order(:name).page(params[:category_page]).per(10)
       @users =  User.order(:name).page(params[:user_page]).per(25)
     elsif current_user && current_user.has_role?(:user)
-      @gigs = Gig.where.not(user_id: current_user.id, status: "published")
+      @gigs = Gig.where(status: "published").where.not(user_id: current_user.id)
     end
+  end
+
+  def project
+
   end
 end
