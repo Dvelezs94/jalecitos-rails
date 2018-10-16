9c7f5edbe8c15fdee242c051e4580983f5414de9
Diego Velez <diegovelzs94@gmail.com>
2018-10-16 16:24:38 -0500

    added facebook back to devise

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   end
 
    devise_for :users, controllers: {
-     sessions: 'users/sessions'
+     sessions: 'users/sessions',
+     omniauth_callbacks: "users/omniauth_callbacks"
    }
    resources :users do
      resources :gigs do
