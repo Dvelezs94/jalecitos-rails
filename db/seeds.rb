6229f5767c8c685e4422b660d87ed5088ef5fa1e
Mauricio Andrés Vélez Sánchez <mauriciovelsa99@gmail.com>
2018-10-24 00:43:35 +0000

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 20.times do |x|
-    Category.create do |category|
-      category.name = Faker::Company.industry
+    Category.create! do |category|
+      category.name = Faker::Company.industry + "#{x}"
     end
 end
 
-20.times do
+20.times do |x|
   User.create! do |user|
     user.email = Faker::Internet.email
     user.role = "user"
     user.password = "123456"
     # This is so we dont have to confirm the email on seeds
     user.confirmed_at = Time.now
-    10.times do
+    10.times do |y|
       user.requests.new do |request|
-        request.name = Faker::Hacker.say_something_smart
+        request.name = Faker::Company.industry + "#{x}#{y}"
         request.description = Faker::Lorem.paragraph(30, true)
         request.location = Faker::Address.state
         request.category_id = Faker::Number.between(1, 10)
         request.budget = Faker::Number.between(100, 5000)
       end
     end
-    10.times do
+    10.times do |x|
       user.gigs.new do |gig|
-        gig.name = Faker::Hacker.say_something_smart
+        gig.name = Faker::Company.industry + "#{x}"
         gig.description = Faker::Lorem.paragraph(30, true)
         gig.location = Faker::Address.state
         gig.category_id = Faker::Number.between(1, 10)
