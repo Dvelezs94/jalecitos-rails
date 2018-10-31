1d95052f432b3e19135110cfc8d95734e947c727
Diego Velez <diegovelzs94@gmail.com>
2018-10-31 00:32:32 -0600

    initial commit

--- app/channels/conversation_channel.rb
+++ app/channels/conversation_channel.rb
@@ -9,10 +9,13 @@ class ConversationChannel < ApplicationCable::Channel
   end
 
   def speak(data)
-   message_params = data['message'].each_with_object({}) do |el, hash|
-     hash[el.values.first] = el.values.last
-   end
+    message_params = data['message'].each_with_object({}) do |el, hash|
+      hash[el.values.first] = el.values.last
+    end
 
-   Message.create(message_params)
- end
+    ActionCable.server.broadcast(
+      "conversations-#{current_user.id}",
+      message: message_params
+    )
+  end
 end
