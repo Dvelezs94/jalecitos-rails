d91337f869b06d1708a403ac740baa1a0ac6a52e
Diego Velez <diegovelzs94@gmail.com>
2018-11-01 13:50:08 -0600

    fix last login at and conversation views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :update_sign_in_at_periodically
+  UPDATE_LOGIN_PERIOD = 1.hours
 
   protected
 
@@ -7,4 +9,14 @@ class ApplicationController < ActionController::Base
    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :password, :password_confirmation])
    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :country])
  end
+
+ def update_sign_in_at_periodically
+    # use session cookie to avoid hammering the database
+    if !session[:last_login_update_at] or session[:last_login_update_at] < UPDATE_LOGIN_PERIOD.ago
+      session[:last_login_update_at] = Time.now
+      if user_signed_in? and current_user.current_sign_in_at < 1.minute.ago # prevents double logins
+        sign_in(current_user, :force => true)
+      end
+    end
+  end
 end
--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,4 +1,5 @@
 class ConversationsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_recipient, only: [:index, :create]
   layout 'conversation'
   access user: :all
@@ -33,7 +34,7 @@ class ConversationsController < ApplicationController
   def set_recipient
     if params[:user_id]
       @remote_user = User.friendly.find(params[:user_id]).id
-      if ! @conversation = Conversation.includes(:sender, :recipient, :messages).where("(sender_id = ? OR recipient_id = ?) AND( sender_id = ? OR recipient_id = ?)", current_user.id, current_user.id, @remote_user , @remote_user).first
+      if ! @conversation = Conversation.where("(sender_id = ? OR recipient_id = ?) AND( sender_id = ? OR recipient_id = ?)", current_user.id, current_user.id, @remote_user , @remote_user).first
           #  Create conversation if not found. this can be refactored later
           Conversation.create!(sender_id: current_user.id, recipient_id: @remote_user)
           set_recipient
