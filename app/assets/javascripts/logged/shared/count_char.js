a13e9f6042a8a90963609bb42ccc69be0c226cf8
mauriciovs <mauriciovs@hotmail.com>
2018-11-08 15:53:29 -0600

    Added color to count label in forms of create and edit gigs and requests

--- app/assets/javascripts/logged/shared/count_char.js
+++ app/assets/javascripts/logged/shared/count_char.js
@@ -1,11 +1,21 @@
 $(document).on('turbolinks:load', function() {
   if ((window.location.href.indexOf("gigs") > -1 || window.location.href.indexOf("requests") > -1) && (window.location.href.indexOf("edit") > -1 || window.location.href.indexOf("new"))) {
 
+    function textColor(length){
+      if (length <= 1000){
+        $("#count")[0].style.color = "green";
+      }
+      else{
+        $("#count")[0].style.color = "red";
+      }
+    }
+
     $(".trix-input").keyup(function() {
       input = $(this).val();
       noHtml = input.replace(/<[^>]*>/g, "");
       noHtml = noHtml.replace(/&nbsp/g, "");
       $("#count").text((1000 - noHtml.length) + "/1000");
+      textColor(noHtml.length);
     });
 
     $(".button_groups").click(function() {
@@ -22,7 +32,7 @@ $(document).on('turbolinks:load', function() {
     });
 
 
-    $( "#trix-toolbar-1" ).after( "<label id='count'></label>" );
+    $( ".trix-input" ).before( "<label id='count'></label>" );
     $(".trix-input").keyup();
   }
 });
