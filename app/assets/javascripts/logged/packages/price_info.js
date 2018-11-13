a350627ada6f2012cba84d6e2e37c68f5a909af5
mauriciovs <mauriciovs@hotmail.com>
2018-11-12 18:38:50 -0600

    Added js for price count and changed some styles

---
+++ app/assets/javascripts/logged/packages/price_info.js
@@ -0,0 +1,21 @@
+$(document).on('turbolinks:load', function() {
+  if ($(".pack-price-class").length > 0) {
+    init_price_count();
+  }
+});
+
+function init_price_count() {
+  $(".price-input").keyup(price_calculation);
+  for (var i = 0; i < 3; i++) {
+    priceInput = $(".price-input-" + i)
+    if(priceInput[0].value != ""){
+      priceInput.keyup();
+    }
+  }
+
+}
+function price_calculation() {
+  input = $(this)[0].className.split(" ")[1].match(/\d+$/)[0];
+  number = $(this)[0].value;
+  $("#price-calc-" + input).text((number*1.1).toFixed(2));
+}
