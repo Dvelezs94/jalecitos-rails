3e59801c450a179f64627cd6436fa621a9b5c145
Mauricio Vélez <mauriciovelsa99@gmail.com>
2018-12-27 15:58:54 -0600

    Fixed bug of ajax events outside logged folder

--- db/schema.rb
+++ db/schema.rb
@@ -283,11 +283,9 @@ ActiveRecord::Schema.define(version: 2018_12_26_203055) do
     t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "gigs_id"
     t.bigint "order_id"
     t.bigint "user_id"
     t.integer "status", default: 0
-    t.index ["gigs_id"], name: "index_reviews_on_gigs_id"
     t.index ["order_id"], name: "index_reviews_on_order_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
@@ -384,7 +382,6 @@ ActiveRecord::Schema.define(version: 2018_12_26_203055) do
   add_foreign_key "reports", "users"
   add_foreign_key "requests", "categories"
   add_foreign_key "requests", "users"
-  add_foreign_key "reviews", "gigs", column: "gigs_id"
   add_foreign_key "reviews", "orders"
   add_foreign_key "reviews", "users"
   add_foreign_key "withdrawals", "users"
