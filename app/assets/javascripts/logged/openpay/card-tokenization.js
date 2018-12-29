5f259e4b580c32d905fa8f27f25fe3cee45c7dd8
Mauricio Vélez <mauriciovelsa99@gmail.com>
2018-12-28 18:42:11 -0600

    Fixed card validation

--- app/assets/javascripts/logged/openpay/card-tokenization.js
+++ app/assets/javascripts/logged/openpay/card-tokenization.js
@@ -1,13 +1,22 @@
 $(document).on('turbolinks:load', function() {
   if ($("#cardForm").length > 0) {
     $('#save-card').on('click', function(event) {
-      event.preventDefault();
-      $("#save-card").prop("disabled", true);
-      OpenPay.token.extractFormAndCreate('cardForm', success_callbak, error_callbak);
+      if ($("#cardForm").valid()) {
+        event.preventDefault();
+        $("#save-card").prop("disabled", true);
+        OpenPay.token.extractFormAndCreate('cardForm', success_callbak, error_callbak);
+      }
     });
     var success_callbak = function(response) {
       var token_id = response.data.id;
       $('#token_id').val(token_id);
+      //dont sent card info to server
+      $("#holder_name").prop('disabled', true);
+      $("#card_number").prop('disabled', true);
+      $("#exp_month").prop('disabled', true);
+      $("#exp_year").prop('disabled', true);
+      $("#card_cvv").prop('disabled', true);
+      //submit
       $('#cardForm').submit();
     };
     var error_callbak = function(response) {
