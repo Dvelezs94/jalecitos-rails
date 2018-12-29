eaf8c19238d5e64350a778ad0e386857ceebed35
Diego Velez <diegovelzs94@gmail.com>
2018-12-29 00:20:24 -0600

    add reviews

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -4,22 +4,4 @@ class Rate < ActiveRecord::Base
 
   #attr_accessible :rate, :dimension
 
-  # Run Gig average job to update score
-  after_commit :resource_average, on: :create
-
-  # method to average the gig and the user score
-  def resource_average
-    # check if the rating is for a review
-    if :rateable_type == "Review"
-      # Rate the Gig and User for employee
-      if self.order.employee
-        GigAverageJob.perform_later(self)
-        EmployeeAverageJob.perform_later(self)
-      # Rate User for employer
-      else
-        EmployerAverageJob.perform_later(self)
-      end
-    end
-  end
-  
 end
