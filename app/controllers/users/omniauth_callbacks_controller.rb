a5029d37fe36ed3c9e4446fdcca312ea8ffbd542
Diego Velez <diegovelzs94@gmail.com>
2019-01-21 16:08:48 -0600

    updated env for facebook debug

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,16 +5,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-      p "usuario persistio"
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url, notice: "Error al tratar de acceder"
-      p "usuario no persistio"
     end
   end
 
   def failure
-    p "fallo facebook omniauth"
     redirect_to root_path, notice: "Error, tu registro no pudo ser completado"
   end
 end
