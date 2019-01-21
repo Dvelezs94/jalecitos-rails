bdece059b04fe0e29679ce3dec989e56165e1543
Diego Velez <diegovelzs94@gmail.com>
2019-01-21 17:10:12 -0600

    test removing from_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -114,14 +114,14 @@ class User < ApplicationRecord
       end
     end
    end
-   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
-    end
-   end
+   # def self.from_omniauth(auth)
+   #  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+   #    user.email = auth.info.email
+   #    user.password = Devise.friendly_token[0,20]
+   #    user.name = auth.info.name   # assuming the user model has a name
+   #    user.image = auth.info.image # assuming the user model has an image
+   #  end
+   # end
    # Override the method to support canceled accounts
    def active_for_authentication?
        super && self.active?
