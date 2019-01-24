11e9ceb4e7339f5250c0e8c6aff5a3d0c37f75fa
Diego Velez <diegovelzs94@gmail.com>
2019-01-24 17:40:23 -0600

    lat and lon for fb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,14 +4,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @auth = request.env["omniauth.auth"]
 
     if ! @user = User.find_by_email(@auth.info.email)
-      @user = User.new(provider: @auth.provider, email: @auth.info.email, password: Devise.friendly_token[0,20], name: @auth.info.name, image: @auth.info.image)
+      @user = User.new(provider: @auth.provider, email: @auth.info.email, password: Devise.friendly_token[0,20], name: @auth.info.name, image: @auth.info.image, lat: params[:sign_up_lat], lon: params[:sign_up_lon])
       @user.skip_confirmation!
       @user.save
     end
 
     if @user.persisted?
       sign_in(@user)
-      flash[:notice] = "Bienvenid@!"
+      # flash[:notice] = "Te damos la bienvenida!"
       redirect_to root_path
       # sign_in_and_redirect @user, :event => :authentication
       # set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
