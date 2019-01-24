bd765301daf3ed16f901ef0d7e22bb88b5b6ec01
Diego Velez <diegovelzs94@gmail.com>
2019-01-23 21:32:36 -0600

    added google tag manager

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,5 +94,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'jalecitos.com' }
+  GoogleTagManager.gtm_id = ENV.fetch("GOOGLE_TAG_MANAGER_ID")
 
 end
