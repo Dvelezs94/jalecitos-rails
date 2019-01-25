d3a5cfc525158b77d3cfcf4855a4e0b7c80fc08f
Diego Velez <diegovelzs94@gmail.com>
2019-01-25 14:11:14 -0600

    corb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,8 +12,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       sign_in(@user)
       # flash[:notice] = "Te damos la bienvenida!"
-      #redirect_to root_path
-      sign_in_and_redirect @user, :event => :authentication
+      redirect_to root_path
+      #sign_in_and_redirect @user, :event => :authentication
       # set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
