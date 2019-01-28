a71429fd7521b30b4d35a27747704a7ab845fb83
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-01-28 14:24:36 -0600

    tried to fix image upload

--- app/assets/javascripts/logged/users/image_upload.js
+++ app/assets/javascripts/logged/users/image_upload.js
@@ -1,7 +1,6 @@
 $(document).on('turbolinks:load', function() {
   /* Activating Best In Place */
   if ($(".profile-container")) {
-    jQuery(".best_in_place").best_in_place();
     $('#user_image').fileupload();
   }
 });
