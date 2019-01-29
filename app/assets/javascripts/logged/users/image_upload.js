1edaf7c2dd6715a1a1735e6d908b8138d1ebbe24
Diego Velez <diegovelzs94@gmail.com>
2019-01-29 00:16:12 -0600

    fixed user profile image upload

--- app/assets/javascripts/logged/users/image_upload.js
+++
@@ -1,7 +0,0 @@
-$(document).on('turbolinks:load', function() {
-  /* Activating Best In Place */
-  if ($(".profile-container")) {
-     $(".best_in_place").best_in_place();
-     $('#user_image').fileupload();
-  }
-});
