fbc73c5215351a2e5b0d7172524314df2b6fec0e
Diego Velez <diegovelzs94@gmail.com>
2019-01-28 20:19:16 -0600

    fix tag 2

--- ecs-deploy.sh
+++ ecs-deploy.sh
@@ -3,16 +3,16 @@
 echo "Deploying commit $CI_COMMIT_SHORT_SHA on branch $CI_COMMIT_REF_NAME"
 case $CI_COMMIT_REF_NAME in
   master)
-    ecs deploy jalecitos-prod rails -t $IMAGE_TAG --region $AWS_REGION --timeout 300
-    ecs deploy jalecitos-prod sidekiq -t $IMAGE_TAG --region $AWS_REGION --timeout 300
+    ecs deploy jalecitos-prod rails -t ${CI_COMMIT_REF_NAME}-${CI_COMMIT_SHORT_SHA} --region $AWS_REGION --timeout 300
+    ecs deploy jalecitos-prod sidekiq -t ${CI_COMMIT_REF_NAME}-${CI_COMMIT_SHORT_SHA} --region $AWS_REGION --timeout 300
   ;;
   development)
-    ecs deploy jalecitos-dev rails -t $IMAGE_TAG --region $AWS_REGION --timeout 300
-    ecs deploy jalecitos-dev sidekiq -t $IMAGE_TAG --region $AWS_REGION --timeout 300
+    ecs deploy jalecitos-dev rails -t ${CI_COMMIT_REF_NAME}-${CI_COMMIT_SHORT_SHA} --region $AWS_REGION --timeout 300
+    ecs deploy jalecitos-dev sidekiq -t ${CI_COMMIT_REF_NAME}-${CI_COMMIT_SHORT_SHA} --region $AWS_REGION --timeout 300
   ;;
   staging)
-    ecs deploy jalecitos-stage rails -t $IMAGE_TAG --region $AWS_REGION --timeout 300
-    ecs deploy jalecitos-stage sidekiq -t $IMAGE_TAG --region $AWS_REGION --timeout 300
+    ecs deploy jalecitos-stage rails -t ${CI_COMMIT_REF_NAME}-${CI_COMMIT_SHORT_SHA} --region $AWS_REGION --timeout 300
+    ecs deploy jalecitos-stage sidekiq -t ${CI_COMMIT_REF_NAME}-${CI_COMMIT_SHORT_SHA} --region $AWS_REGION --timeout 300
   ;;
 esac
 
