19c45888170f3cc8b77b28bef5c8ba705e72a525
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-01-31 15:09:14 -0600

    Working on user search

--- app/assets/javascripts/logged/autocomplete/autocomplete_search.js
+++ app/assets/javascripts/logged/autocomplete/autocomplete_search.js
@@ -3,15 +3,7 @@ $(document).on('turbolinks:load', function() {
     datumTokenizer: Bloodhound.tokenizers.whitespace,
     queryTokenizer: Bloodhound.tokenizers.whitespace,
     remote: {
-      url: '/autocomplete_search?query=%QUERY',
-      replace: function(url, uriEncodedQuery) {
-
-        model = $('#model_name').val();
-        category = $("#category-select").val();
-        loc = $("#search_autocomplete").val();
-
-        return url.replace("%QUERY", uriEncodedQuery) + '&model_name=' + model + '&category_id=' + category + '&location=' + loc
-      },
+      url: '/user_autocomplete_search?query=%QUERY',
       wildcard: '%QUERY'
     }
   });
