690de3e25dbb64b83780fcf2703c65458f2c2be5
Diego Velez <diegovelzs94@gmail.com>
2019-02-02 19:28:17 -0600

    initial messages change

--- app/assets/javascripts/logged/channels/conversation.js
+++ app/assets/javascripts/logged/channels/conversation.js
@@ -3,7 +3,7 @@ App.conversation = App.cable.subscriptions.create("ConversationChannel", {
   disconnected: function() {},
   received: function(data) {
     var conversation = $('#conversation-list').find("[data-conversation-id='" + data['conversation_id'] + "']");
-    conversation.find('.messages-list').find('ul').append(data['message']);
+    conversation.find('.messages-list').append(data['message']);
 
     var messages_list = conversation.find('.messages-list');
     var height = messages_list[0].scrollHeight;
