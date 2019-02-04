a1807c913eb6af828e9f5521c920bcc72c06cf8c
Diego Velez <diegovelzs94@gmail.com>
2019-02-03 21:28:14 -0600

    added jquery to detect mobile on conversations

---
+++ app/assets/javascripts/main/read_query_params.js
@@ -0,0 +1,18 @@
+// this function is used to read query params on the url, you use it like this
+// $.getUrlVar("user_id")
+$.extend({
+  getUrlVars: function(){
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+      hash = hashes[i].split('=');
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+    }
+    return vars;
+  },
+  getUrlVar: function(name){
+    return $.getUrlVars()[name];
+  }
+});
