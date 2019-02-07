29d32a9b7cad5f71d995ef6751c7dfa74dd5f5d2
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-02-06 19:37:59 -0600

    added liked carousel

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,14 @@
 class Like < ApplicationRecord
+  #search
+  searchkick language: "spanish"
+  def search_data
+    {
+      user_id: user_id,
+      gig_id: gig_id,
+      created_at: created_at
+     }
+  end
   belongs_to :user
   belongs_to :gig
+
 end
