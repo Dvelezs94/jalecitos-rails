bcf5f6d7ceda74b3570df70751f6092ada0a3a56
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-02-12 13:45:47 -0600

    fixed bugs with locations and gig partials

--- app/assets/javascripts/logged/shared/google_functions.js
+++ app/assets/javascripts/logged/shared/google_functions.js
@@ -8,10 +8,17 @@ function activatePlacesSearch() {
     }
   };
   var search_autocomplete = new google.maps.places.Autocomplete(search_input, options);
-  search_autocomplete.addListener('place_changed', function(){fill(this, search_input);});
+  search_autocomplete.addListener('place_changed', function() {
+    fill_and_submit(this, search_input);
+  });
+  generate_event("#search_autocomplete");
+
   if (form_input) {
     var form_autocomplete = new google.maps.places.Autocomplete(form_input, options);
-    form_autocomplete.addListener('place_changed', function(){fill(this, form_input);});
+    form_autocomplete.addListener('place_changed', function() {
+      fill_and_submit(this, form_input);
+    });
+    generate_event("#form_autocomplete");
   }
 }
 
@@ -25,12 +32,25 @@ function activateBestSearch() {
         country: "mx"
       }
     };
-    var best_autocomplete = new google.maps.places.Autocomplete(best_input, options)
-    best_autocomplete.addListener('place_changed', function(){fill(this, best_input);});
+    var best_autocomplete = new google.maps.places.Autocomplete(best_input, options);
+    best_autocomplete.addListener('place_changed', function() {
+      fill_and_submit(this, best_input);
+    });
+    generate_event(".best_google_input");
   });
 }
 
-function fill(autocomplete, input) {
+function fill_and_submit(autocomplete, input) {
   var place = autocomplete.getPlace();
   $(input).val([place["address_components"][0].long_name, place["address_components"][1].long_name, place["address_components"][2].long_name].join(", "));
+  //use this just for best_in_place
+  $(input).closest("form.form_in_place").submit();
+}
+
+function generate_event(id_or_class) {
+  $('body').on('keydown', id_or_class, function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+    }
+  });
 }
