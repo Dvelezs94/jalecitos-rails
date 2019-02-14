e5ece337507f9267b2dd81d3a418ef7193be7124
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-02-14 12:43:09 -0600

    stable version before removing best in places inputs of location

--- app/assets/javascripts/logged/autocomplete/autocomplete_location.js
+++ app/assets/javascripts/logged/autocomplete/autocomplete_location.js
@@ -1,19 +1,14 @@
 $(document).on('turbolinks:load', function() {
-  var autocomplete_search = new Bloodhound({
-    datumTokenizer: Bloodhound.tokenizers.whitespace,
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    remote: {
-      url: '/autocomplete_location?query=%QUERY',
-      wildcard: '%QUERY'
-    }
-  });
-
 
   $('#search_autocomplete').typeahead(null, {
-    source: autocomplete_search
+    source: window.autocomplete_location
+  });
+
+  $('.mobile_menu_autocomplete').typeahead(null, {
+    source: window.autocomplete_location
   });
 
   $('#form_autocomplete').typeahead(null, {
-    source: autocomplete_search
+    source: window.autocomplete_location
   });
 });
--- app/assets/javascripts/logged/autocomplete/best_location.js
+++ app/assets/javascripts/logged/autocomplete/best_location.js
@@ -1,6 +1,16 @@
 $(document).on('turbolinks:load', function() {
-  $(".mobile-google-span").click(function() {
-    $(".mobile-location-item").css("display", "none");
-    activateMobileSearch();
-  });
+  if ($(".menu-location-span").length > 0) {
+    $(".menu-location-span").click(function() {
+      $(".mobile-location-item").css("display", "none");
+      mobileMenuAutocomplete();
+    });
+  }
 });
+
+function mobileMenuAutocomplete() {
+  waitForElement(".mobile_menu_autocomplete", function() {
+    $('.mobile_menu_autocomplete').typeahead(null, {
+      source: window.autocomplete_search
+    });
+  });
+}
