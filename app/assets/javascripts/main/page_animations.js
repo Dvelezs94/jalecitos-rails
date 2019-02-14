fd9d4ac3ea5d8882271bd398bd45fb7acf1d4cab
Diego Velez <diegovelzs94@gmail.com>
2019-02-13 20:44:37 -0600

    styles

---
+++ app/assets/javascripts/main/page_animations.js
@@ -0,0 +1,7 @@
+// document.addEventListener('turbolinks:request-start', function() {
+//   $('.main_container').addClass('fadeOut');
+// });
+//
+// $(document).on('turbolinks:render', function() {
+//   $('.main_container')addClass();
+// });
