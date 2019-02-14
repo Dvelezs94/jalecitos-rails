00eb398c6c75b62a0ce8fe6e90396c1046939eff
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-02-13 18:46:05 -0600

    Added mobile search menu and fixed some stuff

--- app/assets/javascripts/logged/autocomplete/autocomplete_search.js
+++ app/assets/javascripts/logged/autocomplete/autocomplete_search.js
@@ -11,5 +11,7 @@ $(document).on('turbolinks:load', function() {
 
   $('#query').typeahead(null, {
     source: autocomplete_search
+  }).on('typeahead:selected', function(e, data) {
+    $(event.target).closest("form").find("input[type=submit]").click();
   });
 });
--- app/assets/javascripts/logged/navbar/toggle_menu.js
+++ app/assets/javascripts/logged/navbar/toggle_menu.js
@@ -1,12 +1,4 @@
 $(document).on('turbolinks:load', function() {
-  $('.menu-open').click(function() {
-    $('.toggle-menu').toggleClass('activee');
-    $('.menu-open').toggleClass('toggle');
-
-  });
-
-
-
   $('#mobile-menu-active').meanmenu({
     meanScreenWidth: "767",
     meanMenuContainer: '.menu-prepent',
