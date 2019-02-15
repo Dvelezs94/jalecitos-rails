555cd0a1a6d42efe4941238223020e8fe2f74d12
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-02-14 19:02:55 -0600

    stable version of location, need to change location in config

--- app/models/concerns/location_validation.rb
+++ app/models/concerns/location_validation.rb
@@ -2,8 +2,7 @@ module LocationValidation
   private
   def location_syntax
     if location != nil
-      valid = location.match(/, México/)
-      valid = location.match(/, Mexico/) if valid.nil?
+      valid = location.match(/, MX/)
       errors.add(:base, "La ubicación debe ser una proporcionada en las opciones.") if valid.nil?
     end
   end
