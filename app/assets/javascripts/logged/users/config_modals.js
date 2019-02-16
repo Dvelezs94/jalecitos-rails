08fe6902f7f1057721e183cdfd39fee28b52738c
Diego Velez <diegovelzs94@gmail.com>
2019-02-15 18:49:36 -0600

    changed config page

--- app/assets/javascripts/logged/users/config_modals.js
+++ app/assets/javascripts/logged/users/config_modals.js
@@ -4,5 +4,10 @@ $(document).on('turbolinks:load', function() {
     modals('bankModal', "bankbtn");
     modals('changePasswordModal', "changepassbtn");
     modals('billingProfileModal', "billingprofilebtn");
+    // buttons for mobile
+    modals('cardModal', "cardbtn2");
+    modals('bankModal', "bankbtn2");
+    modals('changePasswordModal', "changepassbtn2");
+    modals('billingProfileModal', "billingprofilebtn2");
   }
 });
