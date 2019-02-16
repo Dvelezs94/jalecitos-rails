56a47ce4f284d2faa802120d1c77c77921824451
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-02-16 00:30:40 -0600

    fixed search stuff

--- app/assets/javascripts/logged/autocomplete/2_autocomplete_functions.js
+++ app/assets/javascripts/logged/autocomplete/2_autocomplete_functions.js
@@ -8,16 +8,31 @@ $(document).on('turbolinks:load', function() {
 
 function autocom_loc_func(id) {
   //remove info on click
-  $(id).on("focus", function() {
+  $(id).on("focus", function(e) {
     window.location_val = $(this).val();
+    window.city_val = $(e.target).closest("form").find(".city").val()
     $(this).val("");
   });
   //tell the users that they have to select one of the given options if they hit enter and his city_id nis unfilled
-  //(enter doesnt work because form doesnt have submit button)
+  //(sometimes enter doesnt work because form doesnt have submit button)
   $(id).on("keyup", function(e) {
-    if (e.keyCode == 13 && $(e.target).closest("form").find(".city").val() == "") {
+    if (e.keyCode == 13 && $(e.target).closest("form").find(".city").val() == "" && window.accept_nil_loc != true) {
       alert("Debes elegir alguna de las opciones proporcionadas");
       $(this).val("");
+    }
+  });
+  $(id).on("keydown", function(e) {
+    //if value of city is same...
+    if (e.keyCode == 13 && $(e.target).closest("form").find(".city").val() == window.city_val) {
+      //if location is empty, user wants to search in all mexico
+      if ($(this).val() == "" ) {
+        $(e.target).closest("form").find(".city").val("");
+        window.accept_nil_loc = true;
+      }
+      //restore the initial value of location if smething randon is typped
+      else {
+        $(this).val(window.location_val);
+      }
     }
   });
   //retype the location if nothing is set
