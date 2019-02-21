e8a1a610193031023410f1e530e1270cce104f79
Diego Velez <diegovelzs94@gmail.com>
2019-02-21 16:32:19 -0600

    polish stuff

--- app/models/message.rb
+++ app/models/message.rb
@@ -20,5 +20,12 @@ class Message < ApplicationRecord
   validates_presence_of :body, :unless => :image?
   after_create_commit { [MessageBroadcastWorker.perform_async(self.id),  MessageNotificationWorker.perform_in(10.seconds, self.id), MessageEmailWorker.perform_in(10.minutes, self.id)] }
 
+  # update conversation "updated_at" after a message is created
+  after_commit -> {update_conversation(self.conversation)}, on: :create
   mount_uploader :image, MessageUploader
+
+  private
+  def update_conversation (conversation)
+    conversation.touch
+  end
 end
