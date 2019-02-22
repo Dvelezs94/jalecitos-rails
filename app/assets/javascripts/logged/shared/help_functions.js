f52c034af87100d790e6b96952b893fd5886d862
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-02-22 16:11:47 -0600

    now rails-ujs includes jquery_ujs except calling jquery

--- app/assets/javascripts/logged/shared/help_functions.js
+++ app/assets/javascripts/logged/shared/help_functions.js
@@ -19,13 +19,13 @@ function waitForElement(elementPath, callBack) {
 
 function textColor(length, id, number) {
   if (id == "#count_D_" && length <= 1000) {
-    $( id + number)[0].style.color = "green";
+    $( id + number)[0].style.color = "#0067F3";
   }
   else if (id == "#count_D_" && length > 1000){
     $(id + number)[0].style.color = "red";
   }
   else if (id == "#count_N_" && length <= 100){
-    $(id + number)[0].style.color = "green";
+    $(id + number)[0].style.color = "#0067F3";
   }
   else{
     $(id + number)[0].style.color = "red";
