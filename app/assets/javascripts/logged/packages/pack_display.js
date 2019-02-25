e034da6d77700f81b265158a6e29ad1f85d786a0
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-02-25 16:53:39 -0600

    improved package form functions

--- app/assets/javascripts/logged/packages/pack_display.js
+++ app/assets/javascripts/logged/packages/pack_display.js
@@ -1,6 +1,9 @@
 $(document).on('turbolinks:load', function() {
   //if you are in packages form page
   if ($("#packages_form").length == 1) {
+    //show the correct trash
+    displayTrash();
+    //attach the event to the plus
     $("a.pack-tab-displayer").on("click", function() {
       event.preventDefault();
       displayTab();
@@ -19,14 +22,24 @@ $(document).on('turbolinks:load', function() {
 });
 
 function displayTab() {
-  //hide other trash icons
-  $("a.erase-all:visible").css("display", "none");
-  //now display the tab
+  //display the tab
   var hidden_pack = $(".pack-names a.nav-item:hidden");
   hidden_pack[0].style.display = "block";
   hidden_pack[0].click();
   if (hidden_pack.length == 1) {
     $("a.pack-tab-displayer")[0].style.display = "none";
+  }
+  //hide other trash icons
+  displayTrash();
+}
+function displayTrash() {
+  var displayed_navs = $(".package-nav:visible").length - 1;
+  for (i = 0; i < $(".erase-all").length; i++) {
+    if (i == displayed_navs) {
+      $("a.erase-all")[i].style.display = "block";
+    } else {
+      $("a.erase-all")[i].style.display = "none";
+    }
   }
 }
 
@@ -34,9 +47,9 @@ function deleteFormContent(formNumber) {
   if (formNumber > 0) {
     //hide the nav link
     $(".pack-names a.nav-item")[formNumber].style.display = "none";
-    $(".pack-names a.nav-item")[formNumber-1].click();
+    $(".pack-names a.nav-item")[formNumber - 1].click();
     //show the trash icon of earlier form
-    $("a.erase-all")[formNumber-1].style.display = "block";
+    $("a.erase-all")[formNumber - 1].style.display = "block";
 
   }
   $("a.pack-tab-displayer")[0].style.display = "block";
@@ -45,4 +58,6 @@ function deleteFormContent(formNumber) {
   $("trix-editor[input=description" + formNumber + "]")[0].value = "";
   $("trix-editor[input=description" + formNumber + "]").keyup();
   $(".price-input-" + formNumber)[0].value = "";
+  $("#price-calc-" + formNumber).html("-");
+  $("#base-price-" + formNumber).val("");
 }
