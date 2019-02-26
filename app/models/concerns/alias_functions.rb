d61e40f8d8135042292b89549fd45cbe43ca0e79
Diego Velez <diegovelzs94@gmail.com>
2019-02-26 13:37:52 -0600

    fix alias for ever I hope

--- app/models/concerns/alias_functions.rb
+++ app/models/concerns/alias_functions.rb
@@ -2,9 +2,13 @@ module AliasFunctions
   private
   def set_alias
     if self.alias.nil?
-      login_part = self.email.split("@").first.gsub(/[^a-zA-Z0-9\-\_]/,"")
-      hex = SecureRandom.hex(3)
-      self.alias = "#{ login_part }-#{ hex }"
+      begin
+        login_part = self.email.split("@").first.gsub(/[^a-zA-Z0-9\-\_]/,"")
+        hex = SecureRandom.hex(3)
+        self.alias = "#{ login_part }-#{ hex }"
+      rescue
+        self.alias = SecureRandom.hex(4)
+      end
     end
   end
 end
