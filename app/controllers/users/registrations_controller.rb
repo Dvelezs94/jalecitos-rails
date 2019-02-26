db276c7cb1280413e5c1ea30a7b0420e745fe313
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-02-26 14:18:11 -0600

    user score now is created on update always as openpay_id if is nil

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -13,7 +13,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # POST /resource
   def create
     build_resource(sign_up_params.merge(lat: params[:sign_up_lat], lon: params[:sign_up_lon]))
-    resource.save
+    resource.save!
     yield resource if block_given?
     if resource.persisted?
       if resource.active_for_authentication?
