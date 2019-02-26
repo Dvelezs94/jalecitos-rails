05cc0d6be9b8fe5a237361ac7bda2e8c457b9a43
Diego Velez <diegovelzs94@gmail.com>
2019-02-26 12:58:54 -0600

    fix user alias

--- app/models/concerns/alias_functions.rb
+++ app/models/concerns/alias_functions.rb
@@ -2,7 +2,7 @@ module AliasFunctions
   private
   def set_alias
     if self.alias.nil?
-      login_part = self.email.split("@").first
+      login_part = self.email.split("@").first.gsub(/[^a-zA-Z0-9\-\_]/,"")
       hex = SecureRandom.hex(3)
       self.alias = "#{ login_part }-#{ hex }"
     end
