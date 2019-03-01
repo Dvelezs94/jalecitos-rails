db424701c5d9b411d7be2e275db4bee38ba2a439
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-02-28 22:20:35 -0600

    stable version of some improvements

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -6,4 +6,8 @@ class Ticket < ApplicationRecord
   enum status: { in_progress: 0, resolved: 1}
   enum priority: { low: 0, normal: 1, high: 2}
   enum turn: { waiting_for_support: 0, waiting_for_user: 1}
+  #validations
+  validates_presence_of :title, :description, :priority
+  validates_length_of :title, :maximum => 100, :message => "debe contener como máximo 100 caracteres."
+  validates_length_of :description, :maximum => 1000, :message => "debe contener como máximo 1000 caracteres."
 end
