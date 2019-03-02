35fed8807f09e5323cfcdd2a290baa3a590893f6
Fer <fer@la-MacBook-Pro-de-Diego.local>
2019-03-01 18:04:55 -0600

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -329,7 +329,6 @@ ActiveRecord::Schema.define(version: 2019_02_27_225305) do
     t.bigint "category_id"
     t.string "budget"
     t.string "image"
-    t.string "location"
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
