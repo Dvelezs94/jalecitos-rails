4abc6ba69c2b99976f575d78bf6392066a857b91
Diego Velez <diegovelzs94@gmail.com>
2019-03-04 23:54:09 -0600

    added lightbox and images on ticket responses

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -28,6 +28,7 @@ Rails.application.config.assets.precompile += %w( admin.scss )
 Rails.application.config.assets.precompile += %w( mobile.css )
 
 
+
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
