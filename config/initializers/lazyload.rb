40dfcaaedf736558b04f877ddb5306d84edfec22
Diego Velez <diegovelzs94@gmail.com>
2019-03-06 17:19:59 -0600

    fixed lazyload

--- config/initializers/lazyload.rb
+++ config/initializers/lazyload.rb
@@ -1,3 +1,3 @@
 Lazyload::Rails.configure do |config|
-  config.placeholder = "https://www.appelsiini.net/projects/lazyload/img/grey.gif"
+  config.placeholder = "https://s3.us-east-2.amazonaws.com/cdn.jalecitos.com/images/24Mags-loading_grey.gif"
 end
