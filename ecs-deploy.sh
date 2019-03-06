bb733eb28d0dbab5c0d8d85163b014da1f3d9075
Diego Velez <diegovelzs94@gmail.com>
2019-03-06 11:23:19 -0600

    changes deploy script

--- ecs-deploy.sh
+++ ecs-deploy.sh
@@ -3,8 +3,8 @@
 echo "Deploying commit $CI_COMMIT_SHORT_SHA on branch $CI_COMMIT_REF_NAME"
 case $CI_COMMIT_REF_NAME in
   master)
-    ecs deploy jalecitos-prod rails -t ${CI_COMMIT_REF_NAME}-${CI_COMMIT_SHORT_SHA} --region $AWS_REGION --timeout 300
-    ecs deploy jalecitos-prod sidekiq -t ${CI_COMMIT_REF_NAME}-${CI_COMMIT_SHORT_SHA} --region $AWS_REGION --timeout 300
+    ecs deploy jalecitos-production rails -t ${CI_COMMIT_REF_NAME}-${CI_COMMIT_SHORT_SHA} --region $AWS_REGION --timeout 300
+    ecs deploy jalecitos-production sidekiq -t ${CI_COMMIT_REF_NAME}-${CI_COMMIT_SHORT_SHA} --region $AWS_REGION --timeout 300
   ;;
   development)
     ecs deploy jalecitos-dev rails -t ${CI_COMMIT_REF_NAME}-${CI_COMMIT_SHORT_SHA} --region $AWS_REGION --timeout 300
