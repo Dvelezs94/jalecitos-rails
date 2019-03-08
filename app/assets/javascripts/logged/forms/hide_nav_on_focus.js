f5918eecca2e4c06a0b7c832a35eccf8017b5141
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-03-08 16:08:28 -0600

    fixed some stuff

--- app/assets/javascripts/logged/forms/hide_nav_on_focus.js
+++ app/assets/javascripts/logged/forms/hide_nav_on_focus.js
@@ -1,31 +1,12 @@
 $(document).on('turbolinks:load', function() {
   if (screen.width <= 991) {
-    // text areas and inputs
-    $('input').focusin(function() {
-      $('.phone-bottom-menu').hide();
-    });
-    $('input').focusout(function() {
-      $('.phone-bottom-menu').show();
-    });
-    $('textarea').focusin(function() {
-      $('.phone-bottom-menu').hide();
-    });
-    $('textarea').focusout(function() {
-      $('.phone-bottom-menu').show();
-    });
-    //  for best in place
-    $('span.best_in_place').focusin(function() {
-      $('.phone-bottom-menu').hide();
-    });
-    $('span.best_in_place').focusout(function() {
-      $('.phone-bottom-menu').show();
-    });
-    //  for trix
-    document.addEventListener("trix-focus", function(event) {
-      $('.phone-bottom-menu').hide();
-    });
-    document.addEventListener("trix-blur", function(event) {
-      $('.phone-bottom-menu').show();
+    var _originalSize = $(window).width() + $(window).height()
+    $(window).resize(function() {
+      if ($(window).width() + $(window).height() != _originalSize) {
+        $('.phone-bottom-menu').hide(); // keyboard show up
+      } else {
+        $('.phone-bottom-menu').show(); //keyboard closed
+      }
     });
   }
 });
