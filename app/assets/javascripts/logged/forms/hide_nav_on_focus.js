66062e2328d200e158fa0c973821895fbab489ea
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-03-08 16:47:01 -0600

    fixed some stuff

--- app/assets/javascripts/logged/forms/hide_nav_on_focus.js
+++ app/assets/javascripts/logged/forms/hide_nav_on_focus.js
@@ -1,12 +1,15 @@
 $(document).on('turbolinks:load', function() {
+  var _originalSize = $(window).height()
   if (screen.width <= 991) {
-    var _originalSize = $(window).width() + $(window).height()
     $(window).resize(function() {
-      if ($(window).width() + $(window).height() != _originalSize) {
-        $('.phone-bottom-menu').hide(); // keyboard show up
-      } else {
-        $('.phone-bottom-menu').show(); //keyboard closed
+      if ($(window).height() < _originalSize) { // keyboard show up
+        $('.phone-bottom-menu').hide();
+        $(".main_container").css("margin-bottom", "0");
+      } else { //keyboard closed
+        $(".main_container").css("margin-bottom", "9vh");
+        $('.phone-bottom-menu').show();
       }
+      message_view_height(); // use this if conversation is present
     });
   }
 });
