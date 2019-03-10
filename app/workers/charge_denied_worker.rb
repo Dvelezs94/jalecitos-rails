1c30ad5448e9ddfc78cff01218922f647757eee2
Diego Velez <diegovelzs94@gmail.com>
2019-03-09 20:48:42 -0600

    fix job and assetlink

--- app/workers/charge_denied_worker.rb
+++ app/workers/charge_denied_worker.rb
@@ -5,10 +5,14 @@ class ChargeDeniedWorker
 
   def perform(response, error_message)
     @order = Order.find_by_response_order_id(response)
-    if ! @order.waiting_for_bank_approval?
-      return true
+    if defined? @order
+      if ! @order.waiting_for_bank_approval?
+        return true
+      end
+      @order.denied!
+      ChargesMailer.charge_denied(@order, error_message).deliver
+    else
+      true
     end
-    @order.denied!
-    ChargesMailer.charge_denied(@order, error_message).deliver
   end
 end
