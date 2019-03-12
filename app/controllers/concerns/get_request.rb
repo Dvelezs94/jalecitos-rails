e2e17957d4f82f4b561d3f5e80b653f421508f43
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-03-11 19:00:40 -0600

    fixed request view for guest with offers

--- app/controllers/concerns/get_request.rb
+++ app/controllers/concerns/get_request.rb
@@ -1,6 +1,10 @@
 module GetRequest
   private
   def get_other_offers
-    @other_offers = @request.offers.includes(user: :score).where.not(user_id: current_user.id).order(created_at: :desc).page(params[:page]).per(10)
+    if current_user
+      @other_offers = @request.offers.includes(user: :score).where.not(user_id: current_user.id).order(created_at: :desc).page(params[:page]).per(10)
+    else
+      @other_offers = @request.offers.includes(user: :score).order(created_at: :desc).page(params[:page]).per(10)
+    end
   end
 end
