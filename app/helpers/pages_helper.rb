735049ba1e66b88d16211f0d16ec051b112bf637
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-03-14 16:18:11 -0600

    chenged place of voy a

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -28,7 +28,7 @@ module PagesHelper
     if params[:query]
       number_to_currency(purchase_order_total(gig.search_gigs_packages.first.price), precision: 2)
     elsif params[:controller]=="pages" && ! gig.gigs_packages.first.nil?
-      number_to_currency(purchase_order_total(gig.gigs_packages.first.price), precision: 2)
+      number_to_currency(purchase_order_total(gig.gigs_packages.first.price), precision: 0)
     elsif params[:controller]=="users" && ! gig.prof_pack.first.nil?
       number_to_currency(purchase_order_total(gig.prof_pack.first.price), precision: 2)
     elsif params[:controller]=="gigs" && params[:action]=="show" && ! gig.related_pack.first.nil?
