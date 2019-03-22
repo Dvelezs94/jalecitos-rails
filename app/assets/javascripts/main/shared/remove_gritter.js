c12c9b18d9b70c358ee109fd889d439434491477
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-03-22 11:24:41 -0600

    added message of no gigs

--- app/assets/javascripts/main/shared/remove_gritter.js
+++ app/assets/javascripts/main/shared/remove_gritter.js
@@ -3,7 +3,12 @@ $(document).on('turbolinks:before-cache', function() {
 });
 
 $(document).on('turbolinks:load', function() {
-  if(window.cached) { // if page is cached, dont show gritter again
-    $("#gritter-notice-wrapper").addClass("hidden");
+  if (window.cached) { // if page is cached, dont show gritter again (gritter will show just a fer milliseconds)
+    var checkExist = setInterval(function() {
+      if ($('#gritter-notice-wrapper').length) {
+        $("#gritter-notice-wrapper").addClass("hidden");
+        clearInterval(checkExist);
+      }
+    }, 10)
   }
 });
