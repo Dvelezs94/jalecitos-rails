f760b809738641ea1e4065505c8ffc35fa2b3b29
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-03-22 18:19:14 -0600

    added menu effects

---
+++ app/assets/javascripts/main/menu/menu_scroll.js
@@ -0,0 +1,12 @@
+$(document).on('turbolinks:load', function() {
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("main_menu").style.top = "0";
+    } else {
+      document.getElementById("main_menu").style.top = "-150px";
+    }
+    prevScrollpos = currentScrollPos;
+  }
+});
