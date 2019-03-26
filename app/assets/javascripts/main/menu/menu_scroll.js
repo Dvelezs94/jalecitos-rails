ac378eea7741871517f51e4cbd22116e2594d2da
Diego Velez <diegovelzs94@gmail.com>
2019-03-26 17:30:21 -0600

    added fixes for ios

--- app/assets/javascripts/main/menu/menu_scroll.js
+++ app/assets/javascripts/main/menu/menu_scroll.js
@@ -5,14 +5,16 @@ $(document).on('turbolinks:load', function() {
   }
   var prevScrollpos = window.pageYOffset;
   if ( ! isIos()) {
-    window.onscroll = function() {
-      var currentScrollPos = window.pageYOffset;
-      if (prevScrollpos >= currentScrollPos) {
-        document.getElementById("main_menu").style.top = "0";
-      } else {
-        document.getElementById("main_menu").style.top = "-150px";
+    if ($("#configuration-page").length == 0) {
+      window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos >= currentScrollPos) {
+          document.getElementById("main_menu").style.top = "0";
+        } else {
+          document.getElementById("main_menu").style.top = "-150px";
+        }
+        prevScrollpos = currentScrollPos;
       }
-      prevScrollpos = currentScrollPos;
     }
-  }
+   }
 });
