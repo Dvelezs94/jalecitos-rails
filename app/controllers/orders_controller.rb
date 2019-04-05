e2054c09a8051a5d58adbf45851fc2e0f9cbab42
Mauricio Vélez <mauriciovs99@hotmail.com>
2019-04-05 16:21:57 -0600

    fixed flow of orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -121,7 +121,7 @@ class OrdersController < ApplicationController
         flash[:error] = "Hubo un error en tu solicitud"
       end
     end
-    redirect_to finance_path(:table => "purchases", :identifier => @employer_review.id)
+    redirect_to root_path(:identifier => @employer_review.id)
   end
 
 
