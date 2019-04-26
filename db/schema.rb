d5e446e106d5ae50b6aff693854e12bf6bfe121a
Diego Velez <diegovelzs94@gmail.com>
2019-04-25 21:21:14 -0500

    updated redirect when creating card and billing profile

--- db/schema.rb
+++ db/schema.rb
@@ -272,17 +272,6 @@ ActiveRecord::Schema.define(version: 2019_04_25_002921) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "punches", id: :serial, force: :cascade do |t|
-    t.integer "punchable_id", null: false
-    t.string "punchable_type", limit: 20, null: false
-    t.datetime "starts_at", null: false
-    t.datetime "ends_at", null: false
-    t.datetime "average_time", null: false
-    t.integer "hits", default: 1, null: false
-    t.index ["average_time"], name: "index_punches_on_average_time"
-    t.index ["punchable_type", "punchable_id"], name: "punchable_index"
-  end
-
   create_table "push_subscriptions", force: :cascade do |t|
     t.bigint "user_id"
     t.string "auth_key"
