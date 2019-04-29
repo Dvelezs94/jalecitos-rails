9f4d2b81d45e4a4facbea6e4e627ad74c48d23ea
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-04-29 18:37:24 -0500

    fixed form stuff

--- app/assets/javascripts/logged/packages/quantity_restriction.js
+++ app/assets/javascripts/logged/packages/quantity_restriction.js
@@ -3,24 +3,22 @@ $(document).on('turbolinks:load', function() {
   $(".max_amount").keyup(adj_min_amt);
 });
 
-function adj_max_amt () {
+function adj_max_amt() {
   container = $(this).closest("[display_id]");
   max_input = container.find(".max_amount"); //find the input of max_amount
   max_amount = max_input.val(); //check which is the max amount
-  $(this).attr("max", max_amount-1); //adjust max price
-  if ($(this).val() >= max_amount) {
-    show_error("La cantidad mínima de unidades debe ser menor que la máxima");
+  if (max_amount != "") {
+    $(this).attr("max", max_amount - 1); //adjust max price
   }
   $(this).valid(); //display or clean jquery error
 }
 
-function adj_min_amt () {
+function adj_min_amt() {
   container = $(this).closest("[display_id]");
   min_input = container.find(".min_amount"); //find the input of min_amount
   min_amount = min_input.val(); //check which is the value of min input
-  $(this).attr("min", parseInt(min_amount)+1); //adjust min value
-  if ($(this).val() <= min_amount) {
-    show_error("La cantidad máxima de unidades debe ser mayor que la mínima");
+  if (min_amount != "") {
+    $(this).attr("min", parseInt(min_amount) + 1); //adjust min value
   }
   $(this).valid(); //display or clean jquery error
 }
