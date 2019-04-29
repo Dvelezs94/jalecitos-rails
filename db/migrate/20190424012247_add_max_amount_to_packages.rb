62b241bd05c052155b6ac76e7f654f5b0799b5e9
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-04-29 17:47:33 -0500

    finished min amount on form

--- db/migrate/20190424012247_add_max_amount_to_packages.rb
+++ db/migrate/20190424012247_add_max_amount_to_packages.rb
@@ -1,5 +1,7 @@
 class AddMaxAmountToPackages < ActiveRecord::Migration[5.2]
   def change
     add_column :packages, :max_amount, :integer
+    add_column :packages, :min_amount, :integer
+    add_column :packages, :unit_type, :integer
   end
 end
--- db/migrate/20190425002921_add_unit_type_to_packages.rb
+++
@@ -1,5 +0,0 @@
-class AddUnitTypeToPackages < ActiveRecord::Migration[5.2]
-  def change
-    add_column :packages, :unit_type, :integer
-  end
-end
