74429acf737b5dc1be78140ad383ed5fc964af20
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-04-30 16:25:48 -0500

    added more units

--- app/models/package.rb
+++ app/models/package.rb
@@ -6,7 +6,7 @@ class Package < ApplicationRecord
   belongs_to :gig
   #Custom fields
   enum pack_type: { basic: 0, standard: 1, premium: 2}
-  enum unit_type: { piece: 0, dozen: 1, centimeter: 2, meter: 3, kilometer: 4, sec: 5, minute: 6, hour: 7, day: 8}
+  enum unit_type: { piece: 0, dozen: 1, centimeter: 2, meter: 3, kilometer: 4, sec: 5, minute: 6, hour: 7, day: 8, unit: 9, square_centimeter: 10, square_meter: 11 }
 
   # Orders association
   has_many :orders, as: :purchase
