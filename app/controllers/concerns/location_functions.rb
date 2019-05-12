ede9609649e404f89a247da82120ec479c1a0a56
Mauricio Vélez <mauriciovs99@hotmail.com>
2019-05-12 17:02:22 -0500

    added state query

--- app/controllers/concerns/location_functions.rb
+++ app/controllers/concerns/location_functions.rb
@@ -18,14 +18,22 @@ module LocationFunctions
 
   # used in users
   def get_city_id_in_db(city, state, country)
-    @country = Country.find_by_name(country)
-    @state = State.find_by(name: state, country: @country)
-    @city = City.find_by(name: city, state: @state)
-    if @city
-      return @city.id
+    country = Country.find_by_name(country)
+    state = State.find_by(name: state, country: @country)
+    city = City.find_by(name: city, state: @state)
+    if city
+      return city.id
     else
       return nil
     end
   end
+
+  #used in guest queries
+  def get_city_and_state_in_db(city, state, country)
+    @country = Country.find_by_name(country)
+    @state = State.find_by(name: state, country: @country)
+    @city = City.find_by(name: city, state: @state)
+  end
+
 
 end
