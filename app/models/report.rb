5deb26c96fa48a27b17a33e72a871ecae5b2c4d7
Diego Alberto Velez Sanchez <osx@la-MacBook-Pro-de-Diego.local>
2019-05-14 09:44:20 -0500

    fix bans

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,5 +4,5 @@ class Report < ApplicationRecord
   belongs_to :ban, optional: true
   enum status: { open: 0, closed: 1 }
   validates :user_id, :uniqueness => { :scope => [:reportable_type, :reportable_id] }
-  after_commit -> { BanWorker.perform_async(self) }, on: :create
+  after_commit -> { BanWorker.perform_async(self.id) }, on: :create
 end
