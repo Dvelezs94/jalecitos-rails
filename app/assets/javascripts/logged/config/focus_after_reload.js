62441a310e00dd8ba5ada28acce89ae45f7c6d64
Mauricio Vélez <mauriciovs99@hotmail.com>
2019-05-17 14:22:25 -0500

    working on banks workflow

--- app/assets/javascripts/logged/config/focus_after_reload.js
+++ app/assets/javascripts/logged/config/focus_after_reload.js
@@ -2,4 +2,7 @@ $(document).on('turbolinks:load', function() {
   if (focusAfterReload = $.getUrlVar("focusAfterReload")) {
       $("#"+focusAfterReload).focus();
   }
+  if (bestFocusAfterReload = $.getUrlVar("bestFocusAfterReload")) {
+      $("#"+bestFocusAfterReload).click();
+  }
 });
