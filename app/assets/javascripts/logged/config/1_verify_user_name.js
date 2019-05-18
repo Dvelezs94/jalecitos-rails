4c80cca31254e3c8b1e22bdb98cca80bb460c6e7
Mauricio Vélez <mauriciovs99@hotmail.com>
2019-05-17 21:31:39 -0500

    improved flow for user

--- app/assets/javascripts/logged/config/1_verify_user_name.js
+++ app/assets/javascripts/logged/config/1_verify_user_name.js
@@ -5,12 +5,16 @@ $(document).on('turbolinks:load', function() {
       if( $("#change_user_name").hasClass("bip-placeholder") ) { //means no value, using placeholder
         focus_best_span("change_user_name");
         show_error("Asegúrate de tener tu nombre completo en Jalecitos antes de agregar una tarjeta");
+        window.card_after_rename = true;
+        window.bank_after_rename = false;
       }
     });
     $("#bankbtn2").on("click", function() {
       if( $("#change_user_name").hasClass("bip-placeholder") ) { //means no value, using placeholder
         focus_best_span("change_user_name");
         show_error("Asegúrate de tener tu nombre completo en Jalecitos antes de agregar un banco");
+        window.card_after_rename = false;
+        window.bank_after_rename = true;
       }
     });
   }
