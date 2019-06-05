5d3d790bdb9b222ca4e472f4c5cbbf082187bdb5
Mauricio Vélez <mauriciovs99@hotmail.com>
2019-06-04 22:11:12 -0500

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -348,7 +348,6 @@ ActiveRecord::Schema.define(version: 2019_06_04_194128) do
     t.string "reportable_type"
     t.integer "reportable_id"
     t.integer "status", default: 0
-    t.string "cause_str"
     t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -503,6 +502,8 @@ ActiveRecord::Schema.define(version: 2019_06_04_194128) do
     t.datetime "locked_at"
     t.bigint "ally_code_id"
     t.string "session_token"
+    t.boolean "secure_transaction", default: false
+    t.string "secure_transaction_job_id"
     t.index ["ally_code_id"], name: "index_users_on_ally_code_id"
     t.index ["city_id"], name: "index_users_on_city_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
