dd1507581f965fc09110a46b54776aeaa3294066
Mauricio Vélez <mauriciovs99@hotmail.com>
2019-06-04 22:32:02 -0500

    some changes

--- app/workers/notify_refund_worker.rb
+++ app/workers/notify_refund_worker.rb
@@ -6,11 +6,8 @@ class NotifyRefundWorker
   def perform(refund_id)
     @order = Order.find_by_response_refund_id(refund_id)
     if  @order.refunded!
-      #if the order has a dispute (and obviously is disputed...)
-      if @order.dispute
-        #change dispute status to refunded
-        @order.dispute.refunded!
-      end
+      #change dispute status to refunded
+      @order.dispute.refunded! if @order.dispute
     end
     #on worker of payment success if the request is banned, the employee doesnt recieve notification of being hired, because it inmediately gets refunded, so when refunded is successful, employee doesnt need to know that
     employee_has_hire_notification = Notification.find_by(notifiable: @order.purchase, recipient: @order.employee).present?
