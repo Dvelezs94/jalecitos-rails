328cd46061cc8739ab0e5b0e58cdae5bd1694435
Mauricio Vélez <mauriciovelsa99@gmail.com>
2019-06-15 18:52:11 -0500

    some style fixes

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -25,11 +25,11 @@ class CardsController < ApplicationController
     end
 
     ref_params = referer_params(request.referer)
-    if ref_params["package_id"][0] != ""
+    if ref_params["package_id"][0].present?
       quantity = ref_params["quantity"].blank? ? nil : ref_params["quantity"][0]
       package = Package.find_by_slug(ref_params["package_id"])
       redirect_to hire_package_path(package, quantity: quantity)
-    elsif ref_params["offer_id"][0] != ""
+    elsif ref_params["offer_id"][0].present?
       offer = Offer.find_by_id(ref_params["offer_id"])
       redirect_to hire_request_offer_path(offer.request, offer)
     else
