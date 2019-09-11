80277f23047dac3cf7bce4c64e785395a3d9580b
Mauricio Vélez <mauriciovs99@hotmail.com>
2019-09-10 22:51:28 -0500

    added recommendation to gigs

--- app/assets/javascripts/logged/gigs/gigs_modal.js
+++ app/assets/javascripts/logged/gigs/gigs_modal.js
@@ -2,5 +2,5 @@ $(document).on('turbolinks:load', function() {
     modals('report_gig', 'reportObjectModal', "reportgigbtn");
     modals('report_gig', 'reportObjectModal', "reportgigbtndesk");
     modals('recommend_gig', 'recommendationModal', "recommendgigbtn");
-
+    modals('not_recommend_gig', 'recommendationModal', "notrecommendgigbtn");
 });
