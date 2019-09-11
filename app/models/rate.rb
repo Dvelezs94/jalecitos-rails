1f0aaf3f7c2275bc173aea5e451d31da613bfa12
Mauricio Vélez <mauriciovs99@hotmail.com>
2019-09-10 22:51:28 -0500

    added recommendation to gigs

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -2,6 +2,14 @@ class Rate < ActiveRecord::Base
   belongs_to :rater, :class_name => "User"
   belongs_to :rateable, :polymorphic => true
 
+  after_commit :update_review, on: :create, if: :is_recommendation?
   #attr_accessible :rate, :dimension
+
+  def update_review
+    rateable.update(status: "completed")
+  end
+  def is_recommendation?
+    dimension == "Recommendation"? true : false
+  end
 
 end
--- db/migrate/20190909001722_add_recomendation_to_reviews.rb
+++
@@ -1,5 +0,0 @@
-class AddRecomendationToReviews < ActiveRecord::Migration[5.2]
-  def change
-    add_column :reviews, :recomendation, :boolean, default: false
-  end
-end
