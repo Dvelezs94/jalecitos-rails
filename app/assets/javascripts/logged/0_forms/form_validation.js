16566b9cd6686cdfa09febecd35d85111c933a6c
Mauricio Vélez <mauriciovs99@hotmail.com>
2019-09-16 13:19:31 -0500

    improvements

--- app/assets/javascripts/logged/0_forms/form_validation.js
+++ app/assets/javascripts/logged/0_forms/form_validation.js
@@ -34,4 +34,7 @@ $(document).on('turbolinks:load', function() {
   if($(".report_request_form").length > 0){
    $(".report_request_form").validate();
   }
+  if($(".edit_review_form").length > 0){
+   $(".edit_review_form").validate();
+  }
 });
