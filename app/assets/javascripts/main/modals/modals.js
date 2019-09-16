bcfa7accc644e7f166cc15145a2127be13229ae7
Mauricio Vélez <mauriciovs99@hotmail.com>
2019-09-16 00:49:25 -0500

    working on actions for recommendations

--- app/assets/javascripts/main/modals/modals.js
+++ app/assets/javascripts/main/modals/modals.js
@@ -19,13 +19,6 @@ function modals (modalName, modalId, buttonIdOrClass, buttonBehaviour="block", d
     if ($.getUrlVar("modal") == modalName) {
       modal.style.display = "block";
     }
-    // When the user clicks the button, open or close the modal and also closes all other modals (useful when the button behaviour is display the modal)
-    button.on("click", function() {
-      event.preventDefault();
-      if (modalId == "recommendationModal") change_content(buttonIdOrClass)
-      modal.style.display = buttonBehaviour;
-      closeOtherModals(modalId);
-    });
 
     // When the user clicks anywhere outside of the modal, close it
     modal.onclick = function(event) {
@@ -35,6 +28,12 @@ function modals (modalName, modalId, buttonIdOrClass, buttonBehaviour="block", d
       }
     }
   }
+  // When the user clicks the button, open or close the modal and also closes all other modals (useful when the button behaviour is display the modal)
+  $(document).on("click", (buttonIsClass == false)? "#"+buttonIdOrClass : "."+buttonIdOrClass, function() {
+    event.preventDefault();
+    if(modal != null ) modal.style.display = buttonBehaviour;
+    closeOtherModals(modalId);
+  });
 }
 
 function closeOtherModals(modalId){
@@ -43,17 +42,5 @@ function closeOtherModals(modalId){
     if ( $(".modal")[i].id != modalId ){
       $(".modal")[i].style.display = "none";
     }
-  }
-}
-
-function change_content(id) {
-  title = $("#tell_us_recommendation");
-  stars = $("#recommendation_stars");
-  if (id == "recommendgigbtn") {
-    title.html("¿Por qué recomendarías este Jale?");
-    stars.val(5);
-  }else{
-    title.html("¿Por qué no recomendarías este Jale?");
-    stars.val(1);
   }
 }
