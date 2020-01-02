a5997eb179e2b3c8d6f85e1ed4721aae4e0d9bd8
Mauricio Vélez <mauriciovs99@hotmail.com>
2020-01-01 21:50:53 -0600

    stable version of faq

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,4 +1,6 @@
 class Faq < ApplicationRecord
   belongs_to :gig
   validates_presence_of :question, :answer
+  validates_length_of :question, :maximum => 200
+  validates_length_of :answer, :maximum => 1000
 end
