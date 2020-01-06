12f4ef3023dfb7675fe6fe9712119dba79e72320
Mauricio Vélez <mauriciovs99@hotmail.com>
2020-01-06 03:59:05 -0600

    stable version of faq, fixed bug of creating faqs when returns to update gig

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -3,4 +3,12 @@ class Faq < ApplicationRecord
   validates_presence_of :question, :answer
   validates_length_of :question, :maximum => 200
   validates_length_of :answer, :maximum => 1000
+
+  def question=(val)
+    write_attribute(:question, val.strip)
+  end
+
+  def answer=(val)
+    write_attribute(:answer, val.strip)
+  end
 end
