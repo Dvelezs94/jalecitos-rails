833dd831231c5ae5f15220643b15ccc1f0b1bb82
Mauricio Vélez <mauriciovs99@hotmail.com>
2020-01-06 15:19:07 -0600

    whatsapp message now shows every day

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   include GetPages
   access user: :all, admin: [:home], all: [:work, :home, :autocomplete_search, :terms_and_conditions, :privacy_policy, :sales_conditions, :employer_employee_rules, :robots, :sitemap, :install]
   before_action :admin_redirect, only: :home
+  before_action :phone_available, only: :home
   before_action :pending_review, only: [:home], :if => :search_pending_review?
   layout :set_layout
   def home
@@ -91,6 +92,14 @@ class PagesController < ApplicationController
     else
       #get the pending reviews (starting from oldest)
       @p_review = Review.find_by( giver_id: current_user.id, status: "pending" )
+    end
+  end
+
+  def phone_available
+    if current_user && current_user.phone_number.nil?
+      cookies[:phone_available] ||= {
+        expires: (ENV.fetch("RAILS_ENV") == "production")? 1.day: 10.second  #time that elapses to show message again
+      }
     end
   end
 
