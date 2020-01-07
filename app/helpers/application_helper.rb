105e96882503b71f91f2a3b6cf3ce3c1373ac38e
Mauricio Vélez <mauriciovs99@hotmail.com>
2020-01-06 19:02:35 -0600

    changed meta of homepage of guest

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -240,9 +240,9 @@ module ApplicationHelper
         <meta name='category' content='Trabajo, Empleo'>
         <meta property='og:image' content='https://s3.us-east-2.amazonaws.com/cdn.jalecitos.com/images/Logo+PNG.png'>".html_safe
     else
-        "<title>Jalecitos | Encuentra talento freelance en México</title>
-        <meta name='description' content='Servicios Freelance en Mexico'>
-        <meta name='keywords' content='Freelancer, Freelance Mexico, Contratar expertos de confianza, contratar expertos por internet, contratar expertos México, contratar talento por internet'>
+        "<title>Jalecitos | Ofrece tus servicios por internet</title>
+        <meta name='description' content='Ofrece los servicios de tu negocio en la mejor herramienta de marketing digital en México totalmente gratis'>
+        <meta name='keywords' content='marketing digital, ofrecer servicios por internet, sección amarilla, ofrecer servicios, negocio en línea, Freelancer, Freelance Mexico, Contratar expertos de confianza, contratar expertos por internet, contratar expertos México, contratar talento por internet'>
         <meta property='og:image' content='https://s3.us-east-2.amazonaws.com/cdn.jalecitos.com/images/Logo+PNG.png'>".html_safe
     end
   end
