3d476469ce9499830ef825cd761f3ba26aac528c
Diego Velez <diego.sanchez@outcomehealth.com>
2020-01-09 16:29:54 -0600

    added initial nginx setup

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,43 @@
 FROM ruby:2.5.1
-RUN apt-get update -qq && apt-get install -y build-essential libpq-dev curl software-properties-common
+
+RUN apt-get update -qq && apt-get install -y \
+    build-essential \
+    libpq-dev \
+    curl \
+    software-properties-common \
+    nginx \
+    supervisor
+
+
+# Install nodejs
 RUN curl -sL https://deb.nodesource.com/setup_8.x | bash -
 RUN apt install -y nodejs
+
+# Create workspace for app
 RUN mkdir /jalecitos
 WORKDIR /jalecitos
+
+# Copy source files
 COPY . .
+
+# Install gems
 RUN bundle install
+
+# Run asset precompile if CI_AGENT key is set
+RUN if [ -n "$CI_AGENT" ]; then bundle exec rake assets:precompile; fi
+
+# Copy supervisor config files
+COPY .docker/supervisord.conf /etc/supervisor/supervisord.conf
+COPY .docker/supervisor-services.conf /etc/supervisor/conf.d/services.conf
+
+# Copy nginx files
+RUN rm /etc/nginx/sites-enabled/default
+COPY .docker/nginx.conf /etc/nginx/sites-enabled/
+
+# Prevent server pid from saving
 RUN rm -f tmp/pids/server.pid
-CMD bundle exec rails s -p 3000 -b '0.0.0.0'
+
+# cleanup
+RUN rm -rf /var/lib/apt/lists/*
+
+CMD ["/usr/bin/supervisord"]
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,19 +20,19 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_assets = true
-  config.public_file_server.enabled = true
-  config.public_file_server.headers = {
-    'Cache-Control' => "public, s-maxage=#{365.days.to_i}, maxage=#{180.days.to_i}",
-    'Expires' => "#{1.year.from_now.to_formatted_s(:rfc822)}"
-  }
+  # config.serve_static_assets = true
+  # config.public_file_server.enabled = true
+  # config.public_file_server.headers = {
+  #   'Cache-Control' => "public, s-maxage=#{365.days.to_i}, maxage=#{180.days.to_i}",
+  #   'Expires' => "#{1.year.from_now.to_formatted_s(:rfc822)}"
+  # }
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  # config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
