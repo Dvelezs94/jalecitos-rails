ec415ea8708fb43949644937a4fcc174bd7badd1
Mauricio Vélez <mauriciovs99@hotmail.com>
2020-01-08 20:02:22 -0600

    fixed share buttons and faq, added google searchbox

--- app/assets/javascripts/logged/shared/help_functions.js
+++ app/assets/javascripts/logged/shared/help_functions.js
@@ -31,27 +31,3 @@ function textColor(length, id, number) {
     $(id + number)[0].style.color = "red";
   }
 }
-function share_content(elem, desk, text, confirmation, url){
-
-  const canonicalElement = document.querySelector('link[rel=canonical]');
-
-  if (canonicalElement !== null) {
-    url = canonicalElement.href;
-  }
-
-  if (navigator.share) {
-    navigator.share({
-        title: document.title,
-        text: desk,
-        url: url
-    })
-  } else {
-    new Clipboard(elem, {
-      text: function() {
-        return text;
-      }
-    });
-    alert(confirmation);
-  }
-  event.preventDefault();
-}
