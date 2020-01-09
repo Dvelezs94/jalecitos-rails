79abb36721b4727eb99ebe26d7645f3fe3bb29e1
Mauricio Vélez <mauriciovs99@hotmail.com>
2020-01-09 01:55:28 -0600

    added some stuff

--- app/assets/javascripts/guest/shared/sign_modals.js
+++ app/assets/javascripts/guest/shared/sign_modals.js
@@ -1,16 +1,10 @@
 $(document).on('turbolinks:load', function() {
-  modals('sign_in', 'inModal', "sign_in");
-  modals('sign_up', 'upModal', "sign_up");
-  modals('sign_up', 'upModal', "register");
+  modals('sign_in', 'inModal', "sign_in", "block", false, true);
+  modals('sign_up', 'upModal', "sign_up", "block", false, true);
 
   //when resizing on 767 the events doesnt exist because of the menu
   $(window).resize(function() {
-    modals('sign_in', 'inModal', "sign_in");
-    modals('sign_up', 'upModal', "sign_up");
-    modals('sign_up', 'upModal', "register");
+    modals('sign_in', 'inModal', "sign_in", "block", false, true);
+    modals('sign_up', 'upModal', "sign_up", "block", false, true);
   });
-  modals('sign_up', 'upModal', "ready_to");
-
-  modals('sign_up', 'upModal', "nogigmess");
-
 });
