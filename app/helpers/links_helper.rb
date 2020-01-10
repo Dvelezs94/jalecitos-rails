b774f47949919e44cca7ee3a9630cb839b26cf74
Mauricio Vélez <mauriciovs99@hotmail.com>
2020-01-10 12:49:17 -0600

    fixed url regex

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,6 +1,6 @@
 module LinksHelper
   def make_links val
-    regexp = /(https?:\/\/)?[\w,-]*\.\w+(\.\w+)*((?:\/|\?)[^\s]*)?/ #search this regex in other def
+    regexp = /(https?:\/\/)?(www\.)?(\w+\.)[A-Za-z]+((?:\/|\?)[^\s]*)?/ #search this regex in other def
     val.gsub(regexp) { |url|
       url_parsed = URI.parse(url)
       link_url = ( url_parsed.scheme )? url : "http://" + url
