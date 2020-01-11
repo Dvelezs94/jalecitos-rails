6a7bcb1fab9c8eacf9a62db22b63c10d357d3052
Mauricio Vélez <mauriciovs99@hotmail.com>
2020-01-10 22:22:47 -0600

    improved links

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -6,7 +6,7 @@ module LinksHelper
     #1: if url has https, then match url with any extension
     #2: if  url has www, same behaviour as first
     #3: if there is some word that has a dot next to it, then just make url if it is a common extension
-    #this still match https://www.com, but whatsapp and other sites also have this behaviour. Its nonsense trying to make code harder, in 99.9% of cases it would be wasting of time checking for that stuff
+    #this still matches https://www.com, but whatsapp and other sites also have this behaviour. Its nonsense trying to make code harder, in 99.9% of cases it would be wasting of time checking for that stuff
 
     #old regex /(https?:\/\/)?(www\.)?(\w+\.)+[A-Za-z]+[^\s]*/ removed it becuse it matches with stuff like jajaj.yo, if the users dont separate words after dot, then we would have fake urls
     val.gsub(regexp) { |url|
