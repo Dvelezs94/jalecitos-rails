ed65f33ffc9922f43727a35d55549d1f06f7324f
Mauricio Vélez <mauriciovs99@hotmail.com>
2020-01-10 23:39:22 -0600

    improved links

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,13 +1,12 @@
 module LinksHelper
   def make_links val
-    regexp = /((https?:\/\/)(\w+\.)+[A-Za-z]+|(www\.)(\w+\.)+[A-Za-z]+|(\w+\.)+(com|org|net|co|us|mx)(?:(?=(\/|\?|\s))[^\s]*|(?!x)x))[^\s]*/
+    regexp = /((https?:\/\/)(\w+\.)+[A-Za-z]+[^\s]*|(www\.)(\w+\.)+[A-Za-z]+[^\s]*|(\w+\.)+(com|org|net|co|us|mx)(?:(?=(\/|\?|\s))[^\s]*|(?!x)x))/
     #EXPLANATION OF REGEX
     #Look at the two greps | delimiting options in the regex:
     # i made 3 options to match url:
-    #1: if url has https, then match url with any extension
+    #1: if url has https, then match url with any extension and then match everything until space [^\s]* because it may have params
     #2: if  url has www, same behaviour as first
     #3: if there is some word that has a dot next to it, then just make url if it is a common extension, then check if next char is / or ?, or even if the word ends there and match all the word then, otherwise return false((?!x)x always returns false) becaue its a weird extension.All this is useful so google.coma doesnt take as link
-    #for all options, at the end match everything [^\s] (useful in http(s) and www because it means they have params)
 
     #this still matches https://www.com or www.www.com or www.com, but whatsapp and other sites also have this behaviour. Its nonsense trying to make code harder, in 99.9% of cases it would be wasting of time checking for that stuff
 
