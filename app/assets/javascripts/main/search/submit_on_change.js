1ab6483023fa019927a528bfda24db6f3f002cb5
Mauricio Vélez <mauriciovs99@hotmail.com>
2020-03-06 18:05:56 -0600

    pagination and filter now works with ajax (loading in map works), and fixed seeds

--- app/assets/javascripts/main/search/submit_on_change.js
+++ app/assets/javascripts/main/search/submit_on_change.js
@@ -1,6 +1,11 @@
 $(document).on('turbolinks:load', function() {
   $('form#search-form').change(function(event) {
     //dont send form when location changes and not select one
-    if (event.target.id != "search_autocomplete") $(this).closest('form').submit();
+    if (event.target.id != "search_autocomplete") $("#search-form-submit").click();
+  });
+  $( "#search-form-submit" ).on("click", function(event) {
+    $("#searchList").addClass("invisible");
+    $("#loading-results").removeClass("d-none");
+    $("#results-text").html("Cargando...");
   });
 });
