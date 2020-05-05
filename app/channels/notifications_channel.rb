1907775fb34218edf594f4601ca0ae46601dfac3
Mauricio Velez <mauriciovelsa99@gmail.com>
2020-05-04 21:42:33 -0500

    added redis last sign in in notifications channel

--- app/channels/notifications_channel.rb
+++ app/channels/notifications_channel.rb
@@ -1,9 +1,13 @@
 class NotificationsChannel < ApplicationCable::Channel
   def subscribed
     stream_from "notifications-#{current_user.id}"
+    #online users
+    ActionCable.server.pubsub.redis_connection_for_subscriptions.sadd "online", current_user.id
   end
 
   def unsubscribed
     stop_all_streams
+    #online users
+    ActionCable.server.pubsub.redis_connection_for_subscriptions.srem "online", current_user.id
   end
 end
