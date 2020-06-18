aa1769e8eada5a41570a83f86c47ed4b94f16567
Mauricio Velez <mauriciovelsa99@gmail.com>
2020-06-18 17:09:40 -0500

    fixed tagsinput in android

--- app/assets/javascripts/logged/shared/tagsinput.js
+++ app/assets/javascripts/logged/shared/tagsinput.js
@@ -1,7 +1,13 @@
 $(document).on('turbolinks:load', function() {
   $('.tagsinput').tagsinput({
     maxTags: 20,
-    maxChars: 50,
-    confirmKeys: [13, 44]
-  });
+    maxChars: 50
+    });
+    $(".bootstrap-tagsinput").on("keyup", function(e){ //this fixes android bug of comma
+      input = $(this).find("input");
+      if (input.val()[input.val().length-1] == ",") {
+        $("input").blur();
+          $(this).click();
+        }
+    })
 });
