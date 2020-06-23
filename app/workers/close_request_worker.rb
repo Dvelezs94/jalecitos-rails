82289a145048a113370ea7fbfc15c5dec3da21e0
Mauricio Vélez <mauriciovs99@hotmail.com>
2020-06-23 14:06:32 -0500

    modified request worker

--- app/workers/close_request_worker.rb
+++ app/workers/close_request_worker.rb
@@ -3,7 +3,7 @@ class CloseRequestWorker
   sidekiq_options retry: false, dead: false
 
   def perform()
-    Request.where("? > created_at", Time.now-30.days).published do
+    Request.published.where("? > created_at", Time.now-30.days) do
     request.closed!
   end
 end
