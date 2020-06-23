fc2d0fb169af0ebceeaa37d4a704c50f12777b07
Diego Velez <diego.velez@bairesdev.com>
2020-06-23 09:45:07 -0500

    fix sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,6 +1,6 @@
 # Set the host name for URL creation
 SitemapGenerator::Interpreter.send :include, ApplicationHelper
-SitemapGenerator::Sitemap.default_host = "https://www.jalecitos.com"
+SitemapGenerator::Sitemap.default_host = "https://app.wandapp.com.mx"
 SitemapGenerator::Sitemap.compress = false
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(fog_provider: 'AWS',
                                                                     use_iam_profile: true,
@@ -10,7 +10,7 @@ SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(fog_provider
 SitemapGenerator::Sitemap.public_path = 'tmp/'
 SitemapGenerator::Sitemap.sitemaps_host = "https://#{ENV.fetch('S3_BUCKET_NAME') {""}}.s3.amazonaws.com/"
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
-SitemapGenerator::Sitemap.ping_search_engines('https://www.jalecitos.com/sitemap')
+SitemapGenerator::Sitemap.ping_search_engines('https://app.wandapp.com.mx/sitemap')
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
   #
