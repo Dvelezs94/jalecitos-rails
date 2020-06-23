f357f7a41cd64b55311e3c20ee73ebe090d5ac39
Diego Velez <diego.velez@bairesdev.com>
2020-06-23 13:54:07 -0500

    change naming

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  Rails.application.routes.default_url_options[:host] = "https://#{ENV.fetch("ENVNAME")}.jalecitos.com"
-  config.action_mailer.default_url_options = { host: "https://#{ENV.fetch("ENVNAME")}.jalecitos.com" }
+  Rails.application.routes.default_url_options[:host] = "https://#{ENV.fetch("ENVNAME")}.wandapp.com.mx"
+  config.action_mailer.default_url_options = { host: "https://#{ENV.fetch("ENVNAME")}.wandapp.com.mx" }
   GoogleTagManager.gtm_id = ENV.fetch("GOOGLE_TAG_MANAGER_ID")
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  Rails.application.routes.default_url_options[:host] = 'https://www.jalecitos.com'
-  config.action_mailer.default_url_options = { host: 'https://www.jalecitos.com' }
+  Rails.application.routes.default_url_options[:host] = 'https://app.wandapp.com.mx'
+  config.action_mailer.default_url_options = { host: 'https://app.wandapp.com.mx' }
   GoogleTagManager.gtm_id = ENV.fetch("GOOGLE_TAG_MANAGER_ID")
 end
