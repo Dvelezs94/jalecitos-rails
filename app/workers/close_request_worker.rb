696172705149c87f16ccae7a25592b93f8c38686
Mauricio Vélez <mauriciovs99@hotmail.com>
2020-06-23 14:09:38 -0500

    modified request worker

--- app/workers/close_request_worker.rb
+++ app/workers/close_request_worker.rb
@@ -3,7 +3,7 @@ class CloseRequestWorker
   sidekiq_options retry: false, dead: false
 
   def perform()
-    Request.published.where("? > created_at", Time.now-30.days) do
+    Request.published.where("? > created_at", Time.now-30.days).each do |request|
     request.closed!
   end
 end
--- lib/tasks/cloes_requests.rake
+++
@@ -1,7 +0,0 @@
-task :close_requests => [:environment] do
-  puts "Closing old requests..."
-  Request.all.each do |request|
-    request.closed! if Time.now > (request.created_at + 30.days) && request.published?
-  end
-  puts "Task finished"
-end
