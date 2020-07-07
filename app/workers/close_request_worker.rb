a076255b257aff1f7855632bb0022269a840e6ad
Mauricio Vélez <mauriciovs99@hotmail.com>
2020-07-07 09:10:53 -0500

    changes request worker

--- app/workers/close_request_worker.rb
+++ app/workers/close_request_worker.rb
@@ -5,9 +5,9 @@ class CloseRequestWorker
   def perform()
     Request.published.where("? > created_at", Time.now-30.days).each do |request|
       begin
-        request.update!(status: "closed")
+        @req = request.update(status: "closed")
       rescue => e
-        Bugsnag.notify("Error en request con id:" + request.id.to_s+ " con error "+ e)
+        Bugsnag.notify("Error en request con id:" + request.id.to_s+ " con error "+ @req.errors.full_messages.first)
       end
     end
   end
